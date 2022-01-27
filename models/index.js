376f488d3636a1b3175455981191e8f730c263bf
Halid Adechinan <sounslido@gmail.com>
2022-01-26 22:23:45 -0500

    g

--- models/index.js
+++ models/index.js
@@ -10,6 +10,26 @@ const Feedback = require('./feedback');
 User.hasMany(Item)
 Item.belongsTo(User)
 
+User.hasMany(Order)
+Order.belongsTo(User)
+
+User.hasOne(PurchaseCart)
+PurchaseCart.belongsTo(User)
+
+User.hasMany(Feedback)
+Feedback.belongsTo(User)
+
+Item.hasMany(Feedback)
+Feedback.belongsTo(Item)
+//many to many or one to many?
+Item.belongsToMany(Order)
+Order.belongsToMany(Item)
+
+Item.belongsToMany(PurchaseCart)
+PurchaseCart.belongsToMany(Item)
+
+Category.hasMany(Item)
+Item.belongsTo(Category)
 
 module.exports = {
     Item,
