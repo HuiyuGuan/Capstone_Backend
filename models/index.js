a89196385b2db5275b8519e6e2b3efcdb84ec836
HuiyuGuan <Huiyu.guan20@bcmail.cuny.edu>
2022-01-26 21:51:07 -0500

    update

--- models/index.js
+++ models/index.js
@@ -4,12 +4,32 @@ const database = require('./database');
 const Order = require('./order');
 const Category = require('./category');
 const SellingList = require('./selllingList');
-const feedback = require('./feedback');
-const purchaseCart = require('./purchaseCart');
+const Feedback = require('./feedback');
+const PurchaseCart = require('./purchaseCart');
 
 User.hasMany(Item)
 Item.belongsTo(User)
 
+User.hasMany(Order)
+Order.belongsTo(User)
+
+User.hasOne(PurchaseCart)
+PurchaseCart.belongsTo(User)
+
+User.hasMany(Feedback)
+Feedback.belongsTo(User)
+
+Item.hasMany(Feedback)
+Feedback.belongsTo(Item)
+//many to many or one to many?
+Item.belongsToMany(Order)
+Order.belongsToMany(Item)
+
+Item.belongsToMany(PurchaseCart)
+PurchaseCart.belongsToMany(Item)
+
+Category.hasMany(Item)
+Item.belongsTo(Category)
 
 module.exports = {
     Item,
